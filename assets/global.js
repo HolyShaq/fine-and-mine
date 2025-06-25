c3a5c89fa2cf9d56b3f0c18904ba496948b50a2e
HolyShaq <acebyronehalili@gmail.com>
2025-06-25 21:13:16 +0800

    feat: add highlight links on nav menu (mobile is WIP)

--- assets/global.js
+++ assets/global.js
@@ -72,9 +72,11 @@ document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
   summary.setAttribute('role', 'button');
   summary.setAttribute('aria-expanded', summary.parentNode.hasAttribute('open'));
 
-  if (summary.nextElementSibling.getAttribute('id')) {
-    summary.setAttribute('aria-controls', summary.nextElementSibling.id);
+  const nextEl = summary.nextElementSibling;
+  if (nextEl && nextEl.getAttribute('id')) {
+    summary.setAttribute('aria-controls', nextEl.id);
   }
+
 
   summary.addEventListener('click', (event) => {
     event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
